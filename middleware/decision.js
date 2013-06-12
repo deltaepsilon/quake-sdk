33595a9b1f5dbf9b5a277c01ebbe607ba04ed7ae
Chris Esplin <christopher.esplin@gmail.com>
2013-06-12 12:33:19 -0600

    Repairing decision request_uri param

--- middleware/decision.js
+++ middleware/decision.js
@@ -3,7 +3,8 @@ var conf = require('./../config/convict.js'),
   app = express(),
   request = require('superagent'),
   util = require('./../util.js'),
-  quakeRoot = 'https://' + conf.get('quake_host') + ':' + conf.get('quake_port');
+  quakeRoot = 'https://' + conf.get('quake_host') + ':' + conf.get('quake_port'),
+  quiverRoot = 'http://' + conf.get('quiver_host') + ':' + conf.get('quiver_port');
 
 app.get('/quake-sdk/auth/authorize/decision', function (req, res) {
   var params = req.query,
@@ -16,11 +17,12 @@ app.get('/quake-sdk/auth/authorize/decision', function (req, res) {
     grant_type: 'authorization_code',
     code: params.code,
     client_id: conf.get('client_id'),
-    client_secret: conf.get('client_secret')
+    client_secret: conf.get('client_secret'),
+    redirect_uri: quiverRoot + '/quake-sdk/auth/authorize/decision'
   }).end(function (aerr, ares) {
-    res.setHeader('Content-Type', 'text/json');
-    res.end(JSON.stringify(ares.body));
-  });
+      res.setHeader('Content-Type', 'text/json');
+      res.end(JSON.stringify(ares.body));
+    });
 
 });
 
