f39185fb8718c0ac4c9b64f77fd015fb443a47d7
Chris Esplin <christopher.esplin@gmail.com>
2013-06-13 10:12:22 -0600

    Now validating users via OAuth2

--- middleware/decision.js
+++ middleware/decision.js
@@ -6,8 +6,10 @@ var conf = require('./../config/convict.js'),
   quakeRoot = 'https://' + conf.get('quake_host') + ':' + conf.get('quake_port'),
   quiverRoot = 'http://' + conf.get('quiver_host') + ':' + conf.get('quiver_port');
 
-app.get('/quake-sdk/auth/authorize/decision', function (req, res) {
+app.get('/quake-sdk/auth/authorize/decision/:clientID/:clientSecret', function (req, res) {
   var params = req.query,
+    id = req.params.clientID,
+    secret = req.params.clientSecret,
     cookies = util.getQuakeCookies(req),
     token = request.post(quakeRoot + '/auth/token');
 
@@ -16,9 +18,9 @@ app.get('/quake-sdk/auth/authorize/decision', function (req, res) {
   token.send({
     grant_type: 'authorization_code',
     code: params.code,
-    client_id: conf.get('client_id'),
-    client_secret: conf.get('client_secret'),
-    redirect_uri: quiverRoot + '/quake-sdk/auth/authorize/decision'
+    client_id: id,
+    client_secret: secret,
+    redirect_uri: quiverRoot + '/quake-sdk/auth/authorize/decision/' + id + '/' + secret
   }).end(function (aerr, ares) {
       res.setHeader('Content-Type', 'text/json');
       res.end(JSON.stringify(ares.body));
